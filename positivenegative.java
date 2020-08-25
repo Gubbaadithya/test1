36abb98e9b0019e1f6ec665ab2a7a23dbcbe4404
Gubbaadithya <70189785+Gubbaadithya@users.noreply.github.com>
2020-08-25 10:30:22 +0530

    Add files via upload

---
+++ positivenegative.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Controlflow1a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number:");
+		int num=sc.nextInt();
+		if(num>0)
+			System.out.println("Positive");
+		else if(num==0)
+			System.out.println("Zero");
+		else
+			System.out.println("Negative");
+			
+
+	}
+
+}
+
